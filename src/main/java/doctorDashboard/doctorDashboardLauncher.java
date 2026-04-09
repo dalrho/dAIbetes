7ef2a2f2662f9e9b9f2fc29f522ff53b298d8b09
AJ <angelajahziel@gmail.com>
2026-04-09 20:47:04 +0800

    feat: doctor's dashboard javafxml

---
+++ src/main/java/doctorDashboard/doctorDashboardLauncher.java
@@ -0,0 +1,9 @@
+package doctorDashboard;
+
+import results.resultApplication;
+
+public class doctorDashboardLauncher {
+    public static void main(String[] args) {
+        doctorDashboardApplication.main(args);
+    }}
+
