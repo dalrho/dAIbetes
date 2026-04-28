57bf42fc0add90ea110e469956392c4b3e8e2c70
GeraldAres <Geraldbeyou@gmail.com>
2026-04-28 15:55:33 +0800

    Fixed splash screen

--- src/main/java/splashscreen/splashController.java
+++ src/main/java/splashscreen/splashController.java
@@ -1,20 +1,57 @@
 package splashscreen;
 
 import javafx.animation.FadeTransition;
-import javafx.animation.Interpolator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class splashController {
-    public VBox logo;
 
-    public void initialize(){
-        FadeTransition fade = new FadeTransition();
-        fade.setNode(logo);
-        fade.setDuration(Duration.millis(3000));
-        fade.setInterpolator(Interpolator.LINEAR);
+    @FXML
+    private VBox rootContainer;
+
+    // Runs automatically when FXML loads
+    @FXML
+    public void initialize() {
+        playFadeIn();
+    }
+
+    private void playFadeIn() {
+        rootContainer.setOpacity(0);
+
+        FadeTransition fade = new FadeTransition(Duration.millis(1200), rootContainer);
         fade.setFromValue(0);
         fade.setToValue(1);
+        fade.setCycleCount(1);
+        fade.setAutoReverse(false);
         fade.play();
+    }
+
+    public void handleStart(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("results.fxml")
+            );
+
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource())
+                    .getScene()
+                    .getWindow();
+
+            Scene scene = new Scene(root, 900, 600);
+            stage.setScene(scene);
+            stage.show();
+            System.out.println("NICEWORKING");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("HOY NAAY ERROR ARI");
+        }
     }
 }
