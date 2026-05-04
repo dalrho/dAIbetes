349183930ff9b0d9135381ba0d9b89724494e9b5
GeraldAres <Geraldbeyou@gmail.com>
2026-05-04 17:07:01 +0800

    Added splash,login,register,dashboard screens

--- src/main/java/register/registerLauncher.java
+++
@@ -1,9 +0,0 @@
-package register;
-
-import login.loginApplication;
-
-public class registerLauncher {
-    public static void main(String[] args) {
-        registerApplication.main(args);
-    }
-}
