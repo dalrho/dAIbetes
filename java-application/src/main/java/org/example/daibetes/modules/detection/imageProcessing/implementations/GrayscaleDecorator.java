42a52a25148e0928cfd05716aa4ceb73f1aca3ef
AJ <angelajahziel@gmail.com>
2026-05-07 16:10:46 +0800

    feat: updated grayscaledecorator to handle the image per pixel

--- java-application/src/main/java/org/example/daibetes/modules/detection/imageProcessing/implementations/GrayscaleDecorator.java
+++ java-application/src/main/java/org/example/daibetes/modules/detection/imageProcessing/implementations/GrayscaleDecorator.java
@@ -4,6 +4,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
 import org.example.daibetes.modules.detection.imageProcessing.interfaces.ImagePreprocessor;
 
 public class GrayscaleDecorator extends ImageFilterDecorator{
@@ -16,13 +17,16 @@ public class GrayscaleDecorator extends ImageFilterDecorator{
         Image img = inner.process(input);
         int w = (int) img.getWidth();
         int h = (int) img.getHeight();
-        WritableImage output = new WritableImage(w, h);
-        PixelReader pixelReader = img.getPixelReader();
-        PixelWriter pixelWriter = output.getPixelWriter();
 
-        for(int y = 0; y <h; y++){
-            for(int x = 0; x< w; x++){
-                pixelWriter.setColor(x,y, pixelReader.getColor(x,y).grayscale());
+        WritableImage output = new WritableImage(w, h);
+        PixelReader pr = img.getPixelReader();
+        PixelWriter pw = output.getPixelWriter();
+
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                Color color = pr.getColor(x, y);
+                double gray = (color.getRed() * 0.299) + (color.getGreen() * 0.587) + (color.getBlue() * 0.114);
+                pw.setColor(x, y, new Color(gray, gray, gray, color.getOpacity()));
             }
         }
         return output;
