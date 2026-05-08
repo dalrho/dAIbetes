6c6c49e2006202cbba8fa37db725600966bc4af6
AJ <angelajahziel@gmail.com>
2026-05-08 15:35:41 +0800

    feat: updated app context to handle list of images

--- java-application/src/main/java/org/example/daibetes/app/AppContext.java
+++ java-application/src/main/java/org/example/daibetes/app/AppContext.java
@@ -2,9 +2,13 @@ package org.example.daibetes.app;
 
 import javafx.scene.image.Image;
 
+import java.io.File;
+import java.util.List;
+
 public class AppContext {
     private static AppContext instance;
     private Image selectedImage;
+    private List<File> galleryFiles = new java.util.ArrayList<>();
 
     private AppContext() {}
 
@@ -15,4 +19,8 @@ public class AppContext {
 
     public Image getSelectedImage() { return selectedImage; }
     public void setSelectedImage(Image image) { this.selectedImage = image; }
+
+    public List<java.io.File> getGalleryFiles() { return galleryFiles; }
+    public void setGalleryFiles(List<java.io.File> files) { this.galleryFiles = files; }
+
 }
