03cbdb63f86b33bb1e27acc4a68ce15a0c8b5da3
dalrho <shichiya.amistad@google.com>
2026-05-18 01:02:34 +0800

    chore: delete Hello Application import

--- java-application/src/main/java/org/example/daibetes/modules/detection/ui/ImageProcessorTestLauncher.java
+++ java-application/src/main/java/org/example/daibetes/modules/detection/ui/ImageProcessorTestLauncher.java
@@ -1,7 +1,6 @@
 package org.example.daibetes.modules.detection.ui;
 
 import javafx.application.Application;
-import org.example.daibetes.HelloApplication;
 
 public class ImageProcessorTestLauncher {
     public static void main(String[] args) {
