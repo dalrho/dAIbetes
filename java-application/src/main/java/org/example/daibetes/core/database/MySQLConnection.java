221e1c254ca99c8e29f559cebe8cbebc806f30b6
dalrho <shichiya.amistad@google.com>
2026-05-17 21:42:44 +0800

    feat: remove the hardcoded credentials

--- java-application/src/main/java/org/example/daibetes/core/database/MySQLConnection.java
+++ java-application/src/main/java/org/example/daibetes/core/database/MySQLConnection.java
@@ -1,19 +1,50 @@
 package org.example.daibetes.core.database;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class MySQLConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/dbdaibetes";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
+    private static String URL;
+    private static String USERNAME;
+    private static String PASSWORD;
+
+    static {
+        try {
+            Properties properties = new Properties();
+
+            InputStream input = MySQLConnection.class
+                    .getClassLoader()
+                    .getResourceAsStream("config.properties");
+
+            if (input == null) {
+                throw new RuntimeException("config.properties not found");
+            }
+
+            properties.load(input);
+
+            URL = properties.getProperty("db.url");
+            USERNAME = properties.getProperty("db.username");
+            PASSWORD = properties.getProperty("db.password");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            return DriverManager.getConnection(
+                    URL,
+                    USERNAME,
+                    PASSWORD
+            );
 
         } catch (ClassNotFoundException e) {
             System.out.println("MySQL JDBC Driver not found.");
